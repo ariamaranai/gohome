b41647a24fcddd268b3c3b90b9935a423671feaf
a <106607480+ariamaranai@users.noreply.github.com>
2025-08-15 08:28:12 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,11 +1,3 @@
-chrome.contextMenus.onClicked.addListener((_, tab) =>
+chrome.action.onClicked.addListener(tab =>
   chrome.tabs.update({ url: (new URL(tab.url)).origin + "/" })
-);
-chrome.runtime.onInstalled.addListener(() =>
-  chrome.contextMenus.create({
-    id: "",
-    title: "Go host",
-    contexts: ["page", "frame", "link", "editable", "image", "video"],
-    documentUrlPatterns: ["https://*/*", "http://*/*"]
-  })
 );
